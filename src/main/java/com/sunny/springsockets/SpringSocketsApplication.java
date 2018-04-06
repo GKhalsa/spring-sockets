cb8a3229591dd40b7bf73141ad043dc3d74c8f1f
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2018-04-06 10:08:19 -0500

    add initial file structure

---
+++ src/main/java/com/sunny/springsockets/SpringSocketsApplication.java
@@ -0,0 +1,12 @@
+package com.sunny.springsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocketsApplication.class, args);
+	}
+}
